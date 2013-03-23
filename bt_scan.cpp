e9f22057c2b45df1e506c08595ea6285ad8e8c07
Alex Pennace <alex@pennace.org>
2013-03-22 21:26:18 -0400

    Attribute accessors.

--- bt_scan.cpp
+++ bt_scan.cpp
@@ -1,10 +1,20 @@
 #include <unistd.h>
 #include "bt_errors.h"
+#include "bt_node.h"
 #include "bt_scan.h"
 
 BtreeScan::BtreeScan() {}
 
 BtreeScan::~BtreeScan() {}
+
+BtreeNode* BtreeScan::get_leaf() {
+  return leaf;
+}
+
+Status BtreeScan::set_leaf(BtreeNode* l) {
+  leaf = l;
+  return OK;
+}
 
 Status BtreeScan::getNext(int *i) {
   return OK;
